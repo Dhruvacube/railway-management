0e48b2b7fed7c0b07af772174e14aeeab4724f59
Miku Chan <34207400+Dhruvacube@users.noreply.github.com>
2022-10-30 12:53:16 +0530

    adding of hash function

--- constants.cpp
+++ constants.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <map>
 
-
 //hashing
 const std::string salt = "5213761a294a36227cd81dcc06d446b6"; //It could be anything, it will be helpful in decoding the password stored in the database
 
