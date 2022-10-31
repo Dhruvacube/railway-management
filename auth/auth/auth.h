a303833d8e4f90e38d0e1b6d6dad74edc59aa62a
Miku Chan <34207400+Dhruvacube@users.noreply.github.com>
2022-10-31 15:55:11 +0530

    sample code for login

--- auth/auth/auth.h
+++ auth/auth/auth.h
@@ -1,10 +1,11 @@
 #ifndef AUTH_H
 #define AUTH_H
 #include <string>
+#include "utility/constants.h"
 
 class Auth {
     public:
-        bool login(string username, string password);
+        bool login(string username, string password, login_type login_type_t);
 };
 
 #endif
